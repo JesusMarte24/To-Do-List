c3eb8411b9203cf6058ae94bde018e587ce9c1be
Jesus Marte <jesusmarte2430@gmail.com>
2020-07-18 17:25:47 -0400

    added the function to add a new element to the list

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,38 @@
 {
   let list = document.querySelectorAll(".list--items");
   let modalItem = document.querySelectorAll(".item--modal");
+  let containerList = document.querySelector(".container--list");
+  let button = document.querySelector(".container--bottom--button");
+  let html = `<li class="list--items">
+  <a>things<a>
+  <div class="item--modal">Mark as completed?</div>
+  </li>`;
+  let inputValue = document.querySelector(".container--bottom--value");
 
-  for (let i = 0; i < list.length; i++) {
-    list[i].addEventListener("mouseover", () => {
-      mouseOver();
-    });
+  button.addEventListener("click", () => {
+    let elementList;
+    elementList = html.replace("things", inputValue.value);
+    containerList.insertAdjacentHTML("beforeend", elementList);
+    countElements();
+  });
 
-    list[i].addEventListener("mouseout", () => {
-      mouseOut();
-    });
+  function countElements() {
+    for (let i = 0; i < list.length; i++) {
+      list[i].addEventListener("mouseover", () => {
+        mouseOver();
+      });
 
-    function mouseOver() {
-      modalItem[i].style.display = "flex";
-    }
+      list[i].addEventListener("mouseout", () => {
+        mouseOut();
+      });
 
-    function mouseOut() {
-      modalItem[i].style.display = "none";
+      function mouseOver() {
+        modalItem[i].style.display = "flex";
+      }
+
+      function mouseOut() {
+        modalItem[i].style.display = "none";
+      }
     }
   }
 }
