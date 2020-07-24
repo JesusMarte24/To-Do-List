287134e7eb2ef739f43d6b990c2511972f8cf419
Jesus Marte <jesusmarte2430@gmail.com>
2020-07-23 23:02:20 -0400

    setting IDs to each element

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,53 @@
-{
-  let containerList = document.querySelector(".container--list");
-  let button = document.querySelector(".container--bottom--button");
-  let html = `<li class="list--items">
-  <a>things<a>
-  <div class="item--modal">Mark as completed?</div>
+let containerList = document.querySelector(".container--list");
+let button = document.querySelector(".container--bottom--button");
+let html = `<li class="list--items id="">
+  <i>things<i>
+  <div class="item--modal" id="">Mark as completed?</div>
   </li>`;
-  let inputValue = document.querySelector(".container--bottom--value");
+let inputValue = document.querySelector(".container--bottom--value");
+let elementList;
+let List = [];
 
-  button.addEventListener("click", () => {
-    if (inputValue.value !== "" && inputValue.value !== " ") {
-      let elementList;
-      elementList = html.replace("things", inputValue.value);
-      containerList.insertAdjacentHTML("beforeend", elementList);
-      countElements();
-      inputValue.value = "";
-    }
-  });
-
-  function countElements() {
-    let list = document.querySelectorAll(".list--items");
-    let modalItem = document.querySelectorAll(".item--modal");
-
-    for (let i = 0; i < list.length; i++) {
-      list[i].addEventListener("mouseover", () => {
-        mouseOver();
-      });
-
-      list[i].addEventListener("mouseout", () => {
-        mouseOut();
-      });
-
-      function mouseOver() {
-        modalItem[i].style.display = "flex";
-      }
-
-      function mouseOut() {
-        modalItem[i].style.display = "none";
-      }
-    }
+function addItem() {
+  if (inputValue.value !== "" && inputValue.value !== " ") {
+    elementList = html.replace("things", inputValue.value);
+    containerList.insertAdjacentHTML("beforeend", elementList);
+    countElements();
+    inputValue.value = "";
   }
 }
+
+function countElements() {
+  let ID = document.querySelectorAll(".list--items");
+  let modalItem = document.querySelectorAll(".item--modal");
+
+  for (let i = 0; i < ID.length; i++) {
+    ID[i].addEventListener("mouseover", () => {
+      mouseOver();
+    });
+
+    ID[i].addEventListener("mouseout", () => {
+      mouseOut();
+    });
+
+    function mouseOver() {
+      modalItem[i].style.display = "flex";
+    }
+
+    function mouseOut() {
+      modalItem[i].style.display = "none";
+    }
+
+    List.push({
+      name: inputValue.value,
+      ID: i,
+    });
+  }
+}
+
+button.addEventListener("click", addItem);
+inputValue.addEventListener("keyup", (e) => {
+  if (e.keyCode == 13) {
+    addItem();
+  }
+});
